d4408d4409ab753c3bdac50e3f8ea2ffe0d0c040
Van Linh <vanlinh542000@gmail.com>
2021-04-11 12:24:09 +0700

    Linh update detail

--- src/components/profile/content/index.js
+++ src/components/profile/content/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import './profile.css'
+import './profile.css';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 class Content extends Component {
   render() {
     return (
@@ -8,10 +10,19 @@ class Content extends Component {
           <div className='col-6 basic-info'>
             <div className="row">
               <div className="avt col-3">
-                <img className="avt-img"></img>
+                <img className="avt-img" src="https://ict-imgs.vgcloud.vn/2020/09/01/19/huong-dan-tao-facebook-avatar.jpg"></img>
               </div>
               <div className="col-9 detail">
-                <div className="name">alo</div>
+                <div className="name">Nguyễn Văn Linh</div>
+                <div className="folow-row row mt-2">
+                  <div className="col-6"><strong>100 </strong>lượt theo dõi</div>
+                  <div className="col-6"><strong>0 </strong>đang theo dõi</div>
+                </div>
+                <div className="folow-button mt-2">
+                  <Button type="primary" icon={<PlusOutlined />}>
+                    Search
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
