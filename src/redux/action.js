f138dfe2264a2ff6d8484fc65cae7c5741f8de31
yen-pham <yenpham123456987@gmail.com>
2021-04-21 09:37:32 +0700

    Login

--- src/redux/action.js
+++ src/redux/action.js
@@ -5,8 +5,14 @@ import {
     GET_MOTEL_SUCCESS,
     GET_MOTEL,
     GET_MOTEL_FAIL,
+    LOGIN_SUCCESS,
+    REGISTER,
+    LOGIN_FAIL,
+    REGISTER_ERROR
 } from "./constants";
-import firebase from '../connectFirebase/firebase.utils';
+import firebase, { doCreateUserWithEmailAndPassword, doSignInWithEmailAndPassword, doSignInWithFacebook, signInWithGoogle } from '../connectFirebase/firebase.utils';
+import { auth } from '../connectFirebase/firebase.utils';
+import { LOGOUT } from "./constants";
 
 
 export const getMotelsPendingAction = () => ({
@@ -36,6 +42,26 @@ export const getMotelFailAction = error => ({
 
     payload: error
 });
+export const loginSuccess = data => ({
+    type: LOGIN_SUCCESS,
+    payload: data
+});
+export const registerSuccess = () => ({
+    type: REGISTER,
+    // payload: data
+});
+export const registerError = error => ({
+    type: REGISTER_ERROR,
+    payload: error
+});
+export const loginFail = error => ({
+    type: LOGIN_FAIL,
+    payload: error
+});
+
+export const logout = () => ({
+    type: LOGOUT,
+});
 
 export const getMotelsAction = () => {
     return dispatch => {
@@ -48,6 +74,7 @@ export const getMotelsAction = () => {
         }
     };
 };
+
 export const getMotelAction = (id) => {
     return dispatch => {
         try {
@@ -59,3 +86,58 @@ export const getMotelAction = (id) => {
         }
     };
 };
+export const unsubscribe =()=>{
+    return dispatch =>  auth.onAuthStateChanged(user => 
+        dispatch(loginSuccess(user))
+      );
+}
+export const loginFaceAction = () => {
+    return dispatch => {
+        try {
+            return  doSignInWithFacebook()
+            .then(socialAuthUser => dispatch(loginSuccess(socialAuthUser)))
+        } catch (error) {
+            dispatch(loginFail(error));
+        }
+    };
+}
+export const registerAction = (email,password) => {
+    return dispatch => {
+        try {
+            return doCreateUserWithEmailAndPassword(email,password).then(user=>{alert("Đăng nhập thành công !");registerSuccess()}).catch(error => {   
+                switch(error.code) {
+                  case 'auth/email-already-in-use':
+                    dispatch(registerError('Email đã được sử dụng !'));
+                    break;
+                  case 'auth/weak-password':
+                    dispatch(registerError('Mật khẩu nên có ít nhất 6 kí tự !'));
+                        break;
+               }
+               
+             })
+        } catch (error) {
+        }
+    };
+}
+export const loginGoogleAction = () => {
+    return dispatch => {
+        try {
+            return  signInWithGoogle()
+            .then(socialAuthUser => dispatch(loginSuccess(socialAuthUser)))
+        } catch (error) {
+            dispatch(loginFail(error));
+        }
+    };
+}
+export const loginPassAction = (email, password) => {
+    return dispatch => {
+        try {
+            return  doSignInWithEmailAndPassword(email, password).then(socialAuthUser => dispatch(loginSuccess(socialAuthUser)))
+        } catch (error) {
+            dispatch(loginFail(error));
+        }
+    };
+}
+// export const loginPassAction = () => {
+// }
+// doSignInWithEmailAndPassword(email, password).then(socialAuthUser => {console.log(socialAuthUser);})
